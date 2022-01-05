7f7efb15b11ec0e6fb24cc16e6f31c8a1f667324
Mingchao Liang <m3liang@eng.ucsd.edu>
2022-01-04 22:41:51 -0800

    Update dist_test.py

--- tools/dist_test.py
+++ tools/dist_test.py
@@ -176,26 +176,7 @@ def main():
     for i, data_batch in enumerate(data_loader):
         # if i > 0:
         #     break
-        # print(data_batch.keys())
-        # print(type(data_batch['points']))
-        # print('metadata:', data_batch['metadata'])
-        # print('shape', data_batch['shape'])
-        # print('points shape:', data_batch['points'].shape)
-        # print('voxels shape:', data_batch['voxels'].shape)
-        # print('num_points shape:', data_batch['num_points'].shape)
-        # print(data_batch['num_points'])
-        # print(data_batch['coordinates'])
-        # print(data_batch['coordinates'].shape)
-        # print(data_batch['points'][0 , :])
-        # print(torch.min(data_batch['points'], dim = 0)[0], torch.max(data_batch['points'], dim = 0)[0])
-        # print(torch.min(torch.sum(data_batch['voxels'], dim = 1) / data_batch['num_points'][:, None], dim = 0)[0], 
-        #       torch.max(torch.sum(data_batch['voxels'], dim = 1) / data_batch['num_points'][:, None], dim = 0)[0])
-        # print(data_batch['voxels'][0, :, :])
-        # print(data_batch['voxels'][-1, :, :])
-        # print('0', torch.sum(data_batch['coordinates'][:, 0] == 0))
-        # print('1', torch.sum(data_batch['coordinates'][:, 0] == 1))
-        # print('2', torch.sum(data_batch['coordinates'][:, 0] == 2))
-        # print('3', torch.sum(data_batch['coordinates'][:, 0] == 3))
+
         if i == start:
             torch.cuda.synchronize()
             time_start = time.time()
@@ -204,64 +185,36 @@ def main():
             torch.cuda.synchronize()
             time_end = time.time()
 
+        data = dict(
+            features=torch.sum(data_batch['voxels'], dim = 1, keepdim = True) / data_batch['num_points'][:, None, None],
+            num_voxels=torch.ones_like(data_batch['num_points']),
+            coors=data_batch['coordinates'],
+            batch_size=len(data_batch['num_voxels']),
+            input_shape=data_batch["shape"][0],
+        )
+
         # mask = data_batch['coordinates'][:, 0] == 0
-        # data_batch['voxels'] = data_batch['voxels'][mask, :, :]
-        # data_batch['num_points'] = data_batch['num_points'][mask]
-        # data_batch['coordinates'] = data_batch['coordinates'][mask, :]
-        # data_batch['num_voxels'] = data_batch['num_voxels'][[0], ...]
-        # print('len num_voxels', len(data_batch['num_voxels']))
-        # print('len num_voxels', len(data_batch['num_voxels'].to('cuda')))
-        
-
-        # with torch.no_grad():
-        #     outputs = batch_processor(
-        #         model, data_batch, train_mode=False, local_rank=args.local_rank,
-        #     )
-
-            # data = dict(
-            #     features=data_batch['voxels'].to('cuda'),
-            #     num_voxels=data_batch['num_points'].to('cuda'),
-            #     coors=data_batch['coordinates'].to('cuda'),
-            #     batch_size=len(data_batch['num_voxels']),
-            #     input_shape=data_batch["shape"][0],
-            # )
-            # x, multi_feat = model.extract_feat(data)
-
         # data = dict(
-        #     features=torch.sum(data_batch['voxels'], dim = 1, keepdim = True) / data_batch['num_points'][:, None, None],
-        #     num_voxels=torch.ones_like(data_batch['num_points']),
-        #     coors=data_batch['coordinates'],
+        #     features=torch.sum(data_batch['voxels'][mask, :, :], dim = 1, keepdim = True) / data_batch['num_points'][mask, None, None],
+        #     num_voxels=torch.ones_like(data_batch['num_points'][mask]),
+        #     coors=data_batch['coordinates'][mask, :],
         #     batch_size=len(data_batch['num_voxels']),
         #     input_shape=data_batch["shape"][0],
         # )
         # print(data['features'].shape)
         # print(data['num_voxels'].shape)
-        # print(data['coors'].shape)
-        # print(data['coors'])
-        # print(data['batch_size'])
-        # print(data['input_shape'])
-
-        mask = data_batch['coordinates'][:, 0] == 0
-        data = dict(
-            features=torch.sum(data_batch['voxels'][mask, :, :], dim = 1, keepdim = True) / data_batch['num_points'][mask, None, None],
-            num_voxels=torch.ones_like(data_batch['num_points'][mask]),
-            coors=data_batch['coordinates'][mask, :],
-            batch_size=len(data_batch['num_voxels']),
-            input_shape=data_batch["shape"][0],
-        )
-        # print(data['features'].shape)
-        # print(data['num_voxels'].shape)
+        # print(len(data_batch['num_voxels']))
         # print(data['coors'].shape)
         # print(data['coors'])
         # print(data['batch_size'])
         # print(data['input_shape'])
 
 
-        model = model.cuda()
-        with torch.no_grad():
-            outputs = batch_processor(
-                model, data_batch, train_mode=False, local_rank=args.local_rank,
-            )
+        # model = model.cuda()
+        # with torch.no_grad():
+        #     outputs = batch_processor(
+        #         model, data_batch, train_mode=False, local_rank=args.local_rank,
+        #     )
 
         torch.save(data, os.path.join(args.voxel_dir, data_batch['metadata'][0]['token'] + '.pt'))
 
@@ -273,41 +226,41 @@ def main():
         # print(torch.min(outputs[0]['box3d_lidar'][:, :], dim = 0)[0], torch.max(outputs[0]['box3d_lidar'][:, :], dim = 0)[0])
         
 
-        for output in outputs:
-            token = output["metadata"]["token"]
-            for k, v in output.items():
-                if k not in ["metadata", "shape"]:
-                    output[k] = v.to(cpu_device)
-            detections.update(
-                {token: output,}
-            )
+        # for output in outputs:
+        #     token = output["metadata"]["token"]
+        #     for k, v in output.items():
+        #         if k not in ["metadata", "shape"]:
+        #             output[k] = v.to(cpu_device)
+        #     detections.update(
+        #         {token: output,}
+        #     )
 
         if args.local_rank == 0:
             prog_bar.update()
 
     
-    for k in detections.keys():
-        detections[k].pop('backbone_feat', None)
+    # for k in detections.keys():
+    #     detections[k].pop('backbone_feat', None)
 
-    torch.save(detections, os.path.join(args.work_dir, args.output_path))
+    # torch.save(detections, os.path.join(args.work_dir, args.output_path))
 
-    synchronize()
+    # synchronize()
 
-    all_predictions = all_gather(detections)
+    # all_predictions = all_gather(detections)
 
-    print("\n Total time per frame: ", (time_end -  time_start) / (end - start))
+    # print("\n Total time per frame: ", (time_end -  time_start) / (end - start))
 
-    if args.local_rank != 0:
-        return
+    # if args.local_rank != 0:
+    #     return
 
-    predictions = {}
-    for p in all_predictions:
-        predictions.update(p)
+    # predictions = {}
+    # for p in all_predictions:
+    #     predictions.update(p)
 
-    if not os.path.exists(args.work_dir):
-        os.makedirs(args.work_dir)
+    # if not os.path.exists(args.work_dir):
+    #     os.makedirs(args.work_dir)
 
-    save_pred(predictions, args.work_dir)
+    # save_pred(predictions, args.work_dir)
 
     # result_dict, _ = dataset.evaluation(copy.deepcopy(predictions), output_dir=args.work_dir, testset=args.testset)
 
